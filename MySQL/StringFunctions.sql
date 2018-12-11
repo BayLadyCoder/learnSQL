996ca66eadf8938d35dee0999ff3ab8357d9dfcc
Rachada Bay Chairangsaris <rachada_32@hotmail.com>
2018-12-11 16:37:39 -0500

    Running SQL from an SQL file

---
+++ MySQL/StringFunctions.sql
@@ -0,0 +1,7 @@
+Running SQL file
+
+Ex. if you are in the same directory with this file in your Terminal
+source file_name.sql;
+
+Ex. when your file is inside another folder (testing folder)
+source testing/insert.sql
