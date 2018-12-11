59d57649b8a130d5970e1e72ea31f68229410f84
Rachada Bay Chairangsaris <rachada_32@hotmail.com>
2018-12-11 17:02:02 -0500

    book_shop - loading book-data and read the SQL file to create new books table using source book_shop/book-data.sql;

--- MySQL/StringFunctions.sql
+++ MySQL/StringFunctions.sql
@@ -5,3 +5,10 @@ source file_name.sql;
 
 Ex. when your file is inside another folder (testing folder)
 source testing/insert.sql
+
+Loading Book Data from Colt
+CREATE DATABASE book_shop;
+USE book_shop;
+source book_shop/book-data.sql; -- now we have books table
+DESC books;
+SELECT * FROM books;
