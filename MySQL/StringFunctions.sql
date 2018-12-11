8f8bcc6ec69bb063d958589555ef6d0e6c556dd6
Rachada Bay Chairangsaris <rachada_32@hotmail.com>
2018-12-11 17:16:14 -0500

    CONCAT - SELECT     CONCAT (author_fname, ' ', author_lname) FROM books;

--- MySQL/StringFunctions.sql
+++ MySQL/StringFunctions.sql
@@ -12,3 +12,42 @@ USE book_shop;
 source book_shop/book-data.sql; -- now we have books table
 DESC books;
 SELECT * FROM books;
+
+--------------------------------------------------------------------
+
+CONCAT
+Combine Data for Cleaner Output, combine string
+
+Search on google "mysql string functions" for reference
+
+CONCAT (column, anotherColumn) 
+
+
+Ex.
+CONCAT (fistname, lastname) --> TaylorSwift
+
+Ex.
+CONCAT (fistname, ' ', lastname) --> Taylor Swift
+
+*** need to use with SELECT ... FROM ... ***
+Ex.
+SELECT
+    CONCAT (author_fname, ' ', author_lname) 
+FROM books;
+
+Ex.
+SELECT
+    CONCAT (author_fname, ' ', author_lname) AS 'full name'
+FROM books;
+
+Ex.
+SELECT
+    author_fname AS first, author_lname AS last,
+    CONCAT (author_fname, ' ', author_lname) AS full
+FROM books;
+
+Ex.
+SELECT
+    CONCAT (author_fname, ' ', author_lname) AS full1, 
+    CONCAT (author_fname, ', ', author_lname) AS full2 
+FROM books;
