18d58b47d4ef203c1a99633b5b65dfaa53f6c0d3
BayLadyCoder <rachada_32@hotmail.com>
2018-12-15 15:28:49 -0500

    DISTINCT

---
+++ RefiningSelections.sql
@@ -0,0 +1,32 @@
+add three new books
+INSERT INTO books
+    (title, author_fname, author_lname, released_year, stock_quantity, pages)
+    VALUES ('10% Happier', 'Dan', 'Harris', 2014, 29, 256), 
+           ('fake_book', 'Freida', 'Harris', 2001, 287, 428),
+           ('Lincoln In The Bardo', 'George', 'Saunders', 2017, 1000, 367);
+
+
+--------------------------------------------------------------------
+DISTINCT
+only return the unique selection (no duplicated values)
+
+Ex.
+SELECT DISTINCT author_lname FROM books;
+Result:
+/*
++----------------+
+| author_lname   |
++----------------+
+| Lahiri         |
+| Gaiman         |
+| Eggers         |
+| Chabon         |
+| Smith          |
+| Carver         |
+| DeLillo        |
+| Steinbeck      |
+| Foster Wallace |
+| Harris         |
+| Saunders       |
++----------------+
+*/
