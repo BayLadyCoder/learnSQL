32935b0afd52b58c457fa6e000264cfda84e7f2f
Rachada Bay Chairangsaris <rachada_32@hotmail.com>
2018-12-19 00:33:40 -0500

    AVG() with GROUP BY

--- AggregateFunctions.sql
+++ AggregateFunctions.sql
@@ -396,3 +396,57 @@ Result:
 |          1999.7895 |
 +--------------------+
 */
+--------------------------------------------------------------------
+AVG() with GROUP BY
+
+Calculate the average stock quantity for books released in the same year
+Ex.
+SELECT released_year, AVG(stock_quantity) FROM books
+    GROUP BY released_year;
+Result:
+/*
++---------------+---------------------+
+| released_year | AVG(stock_quantity) |
++---------------+---------------------+
+|          2003 |             66.0000 |
+|          2016 |             43.0000 |
+|          2001 |            134.3333 |
+|          1996 |             97.0000 |
+|          2012 |            154.0000 |
+|          2013 |             26.0000 |
+|          2000 |             68.0000 |
+|          2010 |             55.0000 |
+|          1981 |             23.0000 |
+|          1989 |             12.0000 |
+|          1985 |             49.0000 |
+|          1945 |             95.0000 |
+|          2004 |            172.0000 |
+|          2005 |             92.0000 |
+|          2014 |             29.0000 |
+|          2017 |           1000.0000 |
++---------------+---------------------+
+*/
+
+Find average pages for each author
+Ex.
+SELECT author_fname, author_lname, AVG(pages) FROM books
+    GROUP BY author_lname, author_fname;
+Result:
+/*
++--------------+----------------+------------+
+| author_fname | author_lname   | AVG(pages) |
++--------------+----------------+------------+
+| Jhumpa       | Lahiri         |   244.5000 |
+| Neil         | Gaiman         |   325.6667 |
+| Dave         | Eggers         |   431.0000 |
+| Michael      | Chabon         |   634.0000 |
+| Patti        | Smith          |   304.0000 |
+| Raymond      | Carver         |   351.0000 |
+| Don          | DeLillo        |   320.0000 |
+| John         | Steinbeck      |   181.0000 |
+| David        | Foster Wallace |   336.0000 |
+| Dan          | Harris         |   256.0000 |
+| Freida       | Harris         |   428.0000 |
+| George       | Saunders       |   367.0000 |
++--------------+----------------+------------+
+*/
