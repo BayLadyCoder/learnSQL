e03d0cd9104afde94fa8c0028a17dbafbd6e3da8
Rachada Bay Chairangsaris <rachada_32@hotmail.com>
2018-12-19 16:52:03 -0500

    DECIMAL() - DECIMAL(5, 2)

--- DataTypes.sql
+++ DataTypes.sql
@@ -16,3 +16,27 @@ Ex.
 CHAR(5) 
 value = 'ab'
 CHAR(5) --> 'ab   ' --> ab and three spaces
+-------------------------------------------------------------------------------
+NUMBER 
+
+INT = Whole NUMBER
+DECIMAL = numbers that include demicals
+DECIMAL has two arguments 
+- the first argument is for total numbers of the digit including the decimal places
+- the second argument is for numbers of the digit after the decimals or the dot(.)
+
+Ex.
+DECIMAL(5,2)
+Result:
+999.99
+5 digits long and 2 digits after the decimals
+
+Ex.
+CREATE TABLE items (price DECIMAL(5,2));
+INSERT INTO items(price) 
+    VALUES
+        (7), --> 7.00
+        (43632434), --> 999.99
+        (34.88), --> 34.88
+        (298.999999), --> 299.00 (round up)
+        (1.9999); --> 2.00 (round up)
