cd437240aa92fabcd074eebfb83223e742bec65c
Rachada Bay Chairangsaris <rachada_32@hotmail.com>
2019-01-02 23:53:36 -0500

    GREATER THAN > and >=, True/False, Exercise

--- LogicalOperators.sql
+++ LogicalOperators.sql
@@ -87,3 +87,74 @@ WHERE title NOT LIKE 'W%';
 | Lincoln In The Bardo                      |
 +-------------------------------------------+
 */
+-------------------------------------------------------------------------------
+GREATER THAN >
+
+Ex. -- Select books that released after year 2000
+SELECT title, released_year FROM books
+WHERE released_year > 2000;
+/*
++-------------------------------------------+---------------+
+| title                                     | released_year |
++-------------------------------------------+---------------+
+| The Namesake                              |          2003 |
+| Norse Mythology                           |          2016 |
+| American Gods                             |          2001 |
+| A Hologram for the King: A Novel          |          2012 |
+| The Circle                                |          2013 |
+| Just Kids                                 |          2010 |
+| A Heartbreaking Work of Staggering Genius |          2001 |
+| Coraline                                  |          2003 |
+| Oblivion: Stories                         |          2004 |
+| Consider the Lobster                      |          2005 |
+| 10% Happier                               |          2014 |
+| fake_book                                 |          2001 |
+| Lincoln In The Bardo                      |          2017 |
++-------------------------------------------+---------------+
+*/
+
+Ex. -- Return 1 = True
+SELECT 99 > 1;
+/*
++--------+
+| 99 > 1 |
++--------+
+|      1 |
++--------+
+*/
+
+Ex. -- Return 0 = False
+SELECT 1 > 99;
+/*
++--------+
+| 1 > 99 |
++--------+
+|      0 |
++--------+
+*/
+
+GREATER THAN OR EQUAL TO >=
+
+Ex.
+SELECT title, stock_quantity FROM books
+WHERE stock_quantity >= 100;
+/*
++-------------------------------------------+----------------+
+| title                                     | stock_quantity |
++-------------------------------------------+----------------+
+| A Hologram for the King: A Novel          |            154 |
+| A Heartbreaking Work of Staggering Genius |            104 |
+| Coraline                                  |            100 |
+| Oblivion: Stories                         |            172 |
+| fake_book                                 |            287 |
+| Lincoln In The Bardo                      |           1000 |
++-------------------------------------------+----------------+
+*/
+
+Exercise:
+SELECT 100 > 5; --True
+SELECT -15 > 15; --False
+SELECT 9 > -10; --True
+SELECT 1 > 1; --False
+SELECT 'a' > 'b'; --False // 'a' < 'b' is True // a comes before b
+SELECT 'A' > 'a'; --False // 'A' = 'a' is True // case doesn't matter in SQL
