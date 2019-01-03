e8a60badf40d5847e863ca2fc0ef722c6791a9ac
Rachada Bay Chairangsaris <rachada_32@hotmail.com>
2019-01-02 23:36:11 -0500

    NOT EQUAL !=

---
+++ LogicalOperators.sql
@@ -0,0 +1,59 @@
+The Power of Logical Operators
+
+NOT EQUAL !=
+SELECT everything except something(!=)
+
+Ex.
+SELECT title, released_year FROM books
+WHERE released_year != 2017;
+/*
++-----------------------------------------------------+---------------+
+| title                                               | released_year |
++-----------------------------------------------------+---------------+
+| The Namesake                                        |          2003 |
+| Norse Mythology                                     |          2016 |
+| American Gods                                       |          2001 |
+| Interpreter of Maladies                             |          1996 |
+| A Hologram for the King: A Novel                    |          2012 |
+| The Circle                                          |          2013 |
+| The Amazing Adventures of Kavalier & Clay           |          2000 |
+| Just Kids                                           |          2010 |
+| A Heartbreaking Work of Staggering Genius           |          2001 |
+| Coraline                                            |          2003 |
+| What We Talk About When We Talk About Love: Stories |          1981 |
+| Where I'm Calling From: Selected Stories            |          1989 |
+| White Noise                                         |          1985 |
+| Cannery Row                                         |          1945 |
+| Oblivion: Stories                                   |          2004 |
+| Consider the Lobster                                |          2005 |
+| 10% Happier                                         |          2014 |
+| fake_book                                           |          2001 |
++-----------------------------------------------------+---------------+
+*/
+
+Ex.
+SELECT title, author_lname FROM books
+WHERE author_lname != 'Harris';
+/*
++-----------------------------------------------------+----------------+
+| title                                               | author_lname   |
++-----------------------------------------------------+----------------+
+| The Namesake                                        | Lahiri         |
+| Norse Mythology                                     | Gaiman         |
+| American Gods                                       | Gaiman         |
+| Interpreter of Maladies                             | Lahiri         |
+| A Hologram for the King: A Novel                    | Eggers         |
+| The Circle                                          | Eggers         |
+| The Amazing Adventures of Kavalier & Clay           | Chabon         |
+| Just Kids                                           | Smith          |
+| A Heartbreaking Work of Staggering Genius           | Eggers         |
+| Coraline                                            | Gaiman         |
+| What We Talk About When We Talk About Love: Stories | Carver         |
+| Where I'm Calling From: Selected Stories            | Carver         |
+| White Noise                                         | DeLillo        |
+| Cannery Row                                         | Steinbeck      |
+| Oblivion: Stories                                   | Foster Wallace |
+| Consider the Lobster                                | Foster Wallace |
+| Lincoln In The Bardo                                | Saunders       |
++-----------------------------------------------------+----------------+
+*/
