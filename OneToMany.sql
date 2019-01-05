0e122c17697420d7f83c5b2bcde2b3479cdeeb23
Rachada Bay Chairangsaris <rachada_32@hotmail.com>
2019-01-04 23:56:38 -0500

    ON DELETE CASCADE

--- OneToMany.sql
+++ OneToMany.sql
@@ -409,3 +409,94 @@ GROUP BY first_name, last_name;
 | MISSING   | USER     |      801.22 |
 +-----------+----------+-------------+
 */
+----------------------------------------------------------------------------------------
+ON DELETE CASCADE
+-- allow us to delete customers and automatically delete all the orders which have FOREIGN KEY associate with the customers
+
+-- Like this example, we cannot drop customers table before we drop orders table
+DROP TABLE orders, customers;
+
+CREATE TABLE customers (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(100),
+    last_name VARCHAR(100),
+    email VARCHAR(100)
+);
+
+-- Add ON DELETE CASCADE under FOREIGN KEY and REFERENCES
+CREATE TABLE orders (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    order_date DATE,
+    amount DECIMAL(8,2),
+    customer_id INT,
+    FOREIGN KEY(customer_id) 
+        REFERENCES customers(id)
+        ON DELETE CASCADE
+);
+
+INSERT INTO customers (first_name, last_name, email) 
+VALUES ('Boy', 'George', 'george@gmail.com'),
+       ('George', 'Michael', 'gm@gmail.com'),
+       ('David', 'Bowie', 'david@gmail.com'),
+       ('Blue', 'Steele', 'blue@gmail.com'),
+       ('Bette', 'Davis', 'bette@aol.com');
+       
+INSERT INTO orders (order_date, amount, customer_id)
+VALUES ('2016/02/10', 99.99, 1),
+       ('2017/11/11', 35.50, 1),
+       ('2014/12/12', 800.67, 2),
+       ('2015/01/03', 12.50, 2),
+       ('1999/04/11', 450.25, 5);
+
+SELECT * FROM customers;
+/*
++----+------------+-----------+------------------+
+| id | first_name | last_name | email            |
++----+------------+-----------+------------------+
+|  1 | Boy        | George    | george@gmail.com |
+|  2 | George     | Michael   | gm@gmail.com     |
+|  3 | David      | Bowie     | david@gmail.com  |
+|  4 | Blue       | Steele    | blue@gmail.com   |
+|  5 | Bette      | Davis     | bette@aol.com    |
++----+------------+-----------+------------------+
+*/
+
+SELECT * FROM orders;
+/*
++----+------------+--------+-------------+
+| id | order_date | amount | customer_id |
++----+------------+--------+-------------+
+|  1 | 2016-02-10 |  99.99 |           1 |
+|  2 | 2017-11-11 |  35.50 |           1 |
+|  3 | 2014-12-12 | 800.67 |           2 |
+|  4 | 2015-01-03 |  12.50 |           2 |
+|  5 | 1999-04-11 | 450.25 |           5 |
++----+------------+--------+-------------+
+*/
+
+-- We are going to delete BOY GEORGE (from customers table) and it will delete all Boy George's orders (from orders table) automatically
+DELETE FROM customers WHERE email = 'george@gmail.com';
+
+SELECT * FROM customers;
+/* CUSTOMERS TABLE
++----+------------+-----------+-----------------+
+| id | first_name | last_name | email           |
++----+------------+-----------+-----------------+
+|  2 | George     | Michael   | gm@gmail.com    |
+|  3 | David      | Bowie     | david@gmail.com |
+|  4 | Blue       | Steele    | blue@gmail.com  |
+|  5 | Bette      | Davis     | bette@aol.com   |
++----+------------+-----------+-----------------+
+*/
+
+SELECT * FROM orders;
+/*
+ORDERS TABLE
++----+------------+--------+-------------+
+| id | order_date | amount | customer_id |
++----+------------+--------+-------------+
+|  3 | 2014-12-12 | 800.67 |           2 |
+|  4 | 2015-01-03 |  12.50 |           2 |
+|  5 | 1999-04-11 | 450.25 |           5 |
++----+------------+--------+-------------+
+*/
