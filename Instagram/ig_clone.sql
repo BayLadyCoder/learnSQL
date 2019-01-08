09f8a5eb56e817c6e4e29486b4631b655fd6bb84
Rachada Bay Chairangsaris <rachada_32@hotmail.com>
2019-01-08 01:02:03 -0500

    ig_clone - photos table

--- Instagram/ig_clone.sql
+++ Instagram/ig_clone.sql
@@ -29,3 +29,42 @@ SELECT * FROM users;
 +----+----------+---------------------+
 */
 
+-- -------------------------------------------------------------
+-- photos table
+CREATE TABLE photos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    image_url VARCHAR(100),
+    user_id INT NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+-- adding data to photos table
+INSERT INTO photos (image_url, user_id) VALUES
+('/cat1', 1),
+('/catToys', 2),
+('/catFood', 2);
+/*
++----+-----------+---------+---------------------+
+| id | image_url | user_id | created_at          |
++----+-----------+---------+---------------------+
+|  1 | /cat1     |       1 | 2019-01-08 00:59:15 |
+|  2 | /catToys  |       2 | 2019-01-08 00:59:15 |
+|  3 | /catFood  |       2 | 2019-01-08 00:59:15 |
++----+-----------+---------+---------------------+
+*/
+
+-- Select image_url with username who post it
+SELECT photos.image_url, users.username
+FROM photos
+JOIN users
+    ON user_id = users.id;
+/*
++-----------+----------+
+| image_url | username |
++-----------+----------+
+| /cat1     | Katniss  |
+| /catToys  | Sammy    |
+| /catFood  | Sammy    |
++-----------+----------+
+*/
